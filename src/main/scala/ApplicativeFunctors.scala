f947316de8b238e56c78865e2fea35a222f9b850
lukasz.drygala <lukasz.drygala@ocado.com>
2017-03-09 21:57:04 +0100

    add second argument for toPay

--- src/main/scala/ApplicativeFunctors.scala
+++ src/main/scala/ApplicativeFunctors.scala
@@ -1,26 +1,34 @@
 import scala.concurrent.Future
 import scala.language.higherKinds
-import scala.util.Try
+import scala.util.{Failure, Try}
 import scala.concurrent.ExecutionContext.Implicits.global
 
 object ApplicativeFunctors extends App {
 
   case class Voucher(discount: Double)
-  case class Order(id: Int, totalCost: Double, voucher: Voucher)
+
+  case class Order(id: Int, totalCost: Double)
 
   import cats._
   import cats.implicits._
 
-  def toPay[F[_] : Functor](orderF: F[Order]): F[Double] = orderF.map { order =>
-    order.totalCost - order.voucher.discount
+  val afterDiscount = (order: Order, voucher: Voucher) => {
+    order.totalCost - voucher.discount
   }
 
-  val order = Order(1, 100, Voucher(10))
-  val optionOrder = Option(order)
-  val tryOrder = Try(order)
-  val futureOrder = Future.failed[Order](new Exception("Boom"))
+  def toPay[F[_] : Functor](orderF: F[Order], voucherF: F[Voucher]): F[Double] = {
+    val voucherFunction = orderF.map(afterDiscount.curried)
+    voucherFunction.map { f =>
+      voucherF.map { voucher =>
+        f(voucher)
+      }
+    }
+  }
 
-  println(toPay(optionOrder))
-  println(toPay(tryOrder))
-  println(toPay(futureOrder))
+  val voucher = Voucher(10)
+  val order = Order(1, 100)
+
+  println(toPay(Option(order), Option(voucher)))
+  println(toPay(Try(order), Failure[Voucher](new Exception("Boom"))))
+  println(toPay(Future.failed[Order](new Exception("Boom")), Future.successful(voucher)))
 }
