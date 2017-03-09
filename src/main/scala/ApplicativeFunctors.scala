5860d9e1184d044f2ddafa35aec6d73ab4f0f852
lukasz.drygala <lukasz.drygala@ocado.com>
2017-03-09 11:17:19 +0100

    add toPay function

--- src/main/scala/ApplicativeFunctors.scala
+++ src/main/scala/ApplicativeFunctors.scala
@@ -3,5 +3,11 @@ import scala.language.higherKinds
 
 object ApplicativeFunctors extends App {
 
+  import Domain._
 
+  def toPay(order: Order): Double = order.totalCost - order.voucher.discount
+
+  val order = Order(1, 100, Voucher(10))
+
+  println(toPay(order))
 }
