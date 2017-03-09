da9d8f7ac7dad241fb25a4d1b1a66f114ddf9f79
lukasz.drygala <lukasz.drygala@ocado.com>
2017-03-09 21:42:03 +0100

    make toPay polymorphic, move domain

--- src/main/scala/ApplicativeFunctors.scala
+++ src/main/scala/ApplicativeFunctors.scala
@@ -1,15 +1,26 @@
+import scala.concurrent.Future
 import scala.language.higherKinds
-
+import scala.util.Try
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object ApplicativeFunctors extends App {
 
-  import Domain._
+  case class Voucher(discount: Double)
+  case class Order(id: Int, totalCost: Double, voucher: Voucher)
 
-  def toPay(maybeOrder: Option[Order]): Option[Double] = maybeOrder.map { order =>
+  import cats._
+  import cats.implicits._
+
+  def toPay[F[_] : Functor](orderF: F[Order]): F[Double] = orderF.map { order =>
     order.totalCost - order.voucher.discount
   }
 
   val order = Order(1, 100, Voucher(10))
+  val optionOrder = Option(order)
+  val tryOrder = Try(order)
+  val futureOrder = Future.failed[Order](new Exception("Boom"))
 
-  println(toPay(Option(order)))
+  println(toPay(optionOrder))
+  println(toPay(tryOrder))
+  println(toPay(futureOrder))
 }
